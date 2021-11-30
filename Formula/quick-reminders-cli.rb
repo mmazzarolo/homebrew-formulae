ba654943d579adda44af3e95c354b449849aff0c
mmazzarolo <matteomazzarolo@invisionapp.com>
2021-11-30 23:11:39 +0100

    Updated formula

--- Formula/quick-reminders-cli.rb
+++ Formula/quick-reminders-cli.rb
@@ -2,8 +2,8 @@ class QuickRemindersCli < Formula
   depends_on "keith/formulae/reminders-cli"
 
   homepage "https://github.com/mmazzarolo/quick-reminders-cli"
-  url "https://github.com/mmazzarolo/quick-reminders-cli/releases/download/v1.0.1/quick-reminders.tar.gz"
-  sha256 "b09d6ef190f242b5f9634dd0ee77ff618f04543d68ad561efd41ad5b658117cb"
+  url "https://github.com/mmazzarolo/quick-reminders-cli/releases/download/v1.0.2/quick-reminders.tar.gz"
+  sha256 "6948b416ec3ae2021ede8c6ffb5dc45e2a809b4bb0283be7e329a1782ae748ec"
 
   def install
     bin.install "quick-reminders"
